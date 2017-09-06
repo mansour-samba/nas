4582169a4cb60821d8aaa32d7b6e79643bdf2547
mansour-samba <31709991+mansour-samba@users.noreply.github.com>
2017-09-06 21:10:36 +0000

    Add files via upload

---
+++ home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+		<head>
+			<meta charset='utf-8'/>
+			<title> BIENVENU </title>
+			<link rel='stylesheet' href='style1.css'></link>
+			<script type="text/javascript" src="menu.js"></script>
+			<style>
+				p[id='info']
+				{
+					font-family: 'Palatino linotype';
+					font-size: 180%;
+					margin-left: 5%;
+				}
+			</style>
+		</head>
+
+		<body>
+				<?php include('header.php'); ?>
+				<?php include('menu.php'); ?>
+				</br>
+				<br/>
+				</br>
+				<br/>
+				</br>
+				
+		<p id='info'>
+			Vous etes ici dans l'interface de la base de donnees de l'hopital X. <br/>
+			Cette base a pour principal but de recueillir l'ensemble des informations concernant le personnel administratif et technique de ladite structure. </br>
+			Elle prend en compte aussi, les informations sur les patients ainsi que les interactions qui les impliquent tout au long de leur prise en charge.
+		</p>
+		
+		</br>
+		<br/>
+		</br>
+				
+				<?php include('footer.php'); ?>
+		</body>
+</html>
